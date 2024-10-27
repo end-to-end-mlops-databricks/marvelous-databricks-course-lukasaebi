9155f7c4a6b35320195545df1f26436381c30d48
Lukas Aebi <lukas.aebi@gmx.net>
2024-10-27 13:30:49 +0100

    added classmethod 'from_yaml' to dataconfig

--- reservations/config.py
+++ reservations/config.py
@@ -1,3 +1,4 @@
+import yaml
 from pydantic import BaseModel
 
 
@@ -7,3 +8,10 @@ class DataConfig(BaseModel):
     categorical_variables: list[str]
     test_size: float
     random_state: int
+
+    @classmethod
+    def from_yaml(cls, config_path: str):
+        """Load configuration from a YAML file."""
+        with open(config_path, "r") as f:
+            config_dict = yaml.safe_load(f)
+        return cls(**config_dict)
